cd46026f78c2d73d8105e93b2f2805dd9a8b5024
Moksha <mokshasood06@gmail.com>
2021-02-08 00:03:09 +0530

    added username and secretKey

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+	return (
+		<ChatEngine
+			height="100vh"
+			projectID="1cafd1f3-98fc-49ae-ae2b-8837dd11f01d"
+			userName="jinmokcoders@gmail.com"
+			userSecret="jinmok56"
+		/>
+	);
+}
+
+export default App;
